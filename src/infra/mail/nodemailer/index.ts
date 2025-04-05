513cee23ded79d318a1ec235bebd51288b53c5c1
Orlando Nascimento <orlandocorreia2@hotmail.com>
2025-04-05 01:56:40 -0300

    feat: install playwright chromium

--- src/infra/mail/nodemailer/index.ts
+++ src/infra/mail/nodemailer/index.ts
@@ -1,36 +1,29 @@
 import nodemailer from 'nodemailer';
-import { env } from '../../../utils/env';
+import { env, envBoolean, envNumber } from '../../../utils/env';
 import { SendMailProps } from '../../../types/email';
 import { isEnvironmentProduction } from '../../../utils/helper';
 import path from 'path';
 
-// export const transport = nodemailer.createTransport({
-//   host: 'smtp.kinghost.net',
-//   port: 465,
-//   secure: true, // true for port 465, false for other ports
-//   auth: {
-//     user: 'no-reply@meuleilaoonline.com',
-//     pass: 'J@rvi2025',
-//   },
-// });
-
-const transportConfig = {
-  service: 'kinghost',
+export const transport = nodemailer.createTransport({
   host: 'smtp.kinghost.net',
-  port: 465,
-  secure: true,
-  pool: true,
-  auth: { user: 'no-reply@meuleilaoonline.com', pass: 'J@rvi2025' },
-};
-
-const transport = nodemailer.createTransport(transportConfig);
+  port: 587,
+  secure: false, // true for port 465, false for other ports
+  auth: {
+    user: 'no-reply@meuleilaoonline.com',
+    pass: 'J@rvi2025',
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  logger: true,
+  debug: true,
+});
 
 export const sendMail = ({ to, subject, html, text }: SendMailProps) => {
-  console.log('Valor das variaveis de ambiente', transportConfig);
   const sendTo = isEnvironmentProduction ? to : env({ key: 'MAIL_FROM_DEVS' });
   transport
     .sendMail({
-      from: 'no-reply@meuleilaoonline.com',
+      from: env({ key: 'MAIL_FROM' }),
       to: sendTo,
       subject,
       html,
