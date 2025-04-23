cd843b357e1d24f43b7e6257e369cdbd6c161cbc
Orlando Nascimento <orlando.nascimento@capitanigroup.com.br>
2025-04-23 17:15:29 -0300

    feat: node-fetch

--- src/modules/auction.properties/usecases/create.auction.properties.batch.usecase.ts
+++ src/modules/auction.properties/usecases/create.auction.properties.batch.usecase.ts
@@ -238,7 +238,8 @@ export class CreateAuctionPropertiesBatchUseCase
       'Accept-Encoding': 'gzip, deflate',
       'Accept-Language': 'en-US,en;q=0.9,ms;q=0.8,te;q=0.7',
       'Cache-Control': 'max-age=0',
-      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Type':
+        'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
       'Upgrade-Insecure-Requests': '1',
       'User-Agent':
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)',
