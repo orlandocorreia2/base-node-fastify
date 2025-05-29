d894c5afbb12f0bb8971c66fea8354daa6465082
Orlando Nascimento <orlando.nascimento@capitanigroup.com.br>
2025-05-29 13:16:09 -0300

    feat: add health route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,20 @@ import { permissionGroupRoutesModule } from '../modules/permision.groups/routes'
 import { sessionRoutesModule } from '../modules/sessions/routes';
 import { userRoutesModule } from '../modules/users/routes';
 import { FastifyTypedInstance } from '../types/types';
+import { version } from '../../package.json';
 
 export const appRoutes = (app: FastifyTypedInstance) => {
   userRoutesModule(app);
   sessionRoutesModule(app);
   permissionGroupRoutesModule(app);
   auctionPropertyRoutesModule(app);
+
+  app.get('/health', () => ({
+    status: 'Healthy',
+    uptime: process.uptime(),
+    name: 'Jarvis',
+    version,
+    message: 'Jarvis Api is up!',
+    timestamp: new Date(),
+  }));
 };
